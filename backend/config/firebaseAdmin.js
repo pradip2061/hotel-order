c75b5103e01bf00f4b388303db1c075d2b4f5723
pradip2061 <ps6000146@gmail.com>
2026-01-15 18:21:22 +0545

    fixed error

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,7 +1,21 @@
 const admin = require("firebase-admin");
 
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: "googleapis.com",
+};
+
 admin.initializeApp({
-  credential: admin.credential.cert(require("../serviceAccountKey.json")),
+  credential: admin.credential.cert(require(serviceAccount)),
 });
 
 module.exports = admin;
